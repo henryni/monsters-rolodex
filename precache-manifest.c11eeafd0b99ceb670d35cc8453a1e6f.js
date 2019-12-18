d84d518981bb70797f9838fe2406ecd0865f7535
Henry Ni <yyvome2005tw@gmail.com>
2019-12-18 14:48:15 +0800

    Updates

---
+++ precache-manifest.c11eeafd0b99ceb670d35cc8453a1e6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fcd770e862f6d5ca6d3be7a92e71133",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d9c9bd949c769d6ee3e9",
+    "url": "/monsters-rolodex/static/css/main.ab15933a.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "d9c9bd949c769d6ee3e9",
+    "url": "/monsters-rolodex/static/js/main.bdaaabc4.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
